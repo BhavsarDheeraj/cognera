ed00ddd4b4ca844043d104dca87082f93c7805e3
Dheeraj Bhavsar <dheerajbhavsar940@gmail.com>
2019-06-07 16:08:38 +0530

    Initial Commit

---
+++ Cognera/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Cognera
+//
+//  Created by DHEERAJ BHAVSAR on 07/06/19.
+//  Copyright © 2019 Dheeraj Bhavsar. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
